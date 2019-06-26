62e52a0bf41dec94f9ac46840c288fb9fde61c07
k670 <2ki15b.melnik@gmail.com>
2019-06-26 19:02:42 +0300

    Add files via upload

---
+++ src/main/java/k670/App.java
@@ -0,0 +1,28 @@
+package k670;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
+
+        File file = new File(".\\target\\res\\test.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine()) != null) {
+            System.out.println(st);
+        }
+        System.out.println("Hello World!");
+    }
+}
